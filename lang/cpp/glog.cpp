cd300a90ab347d8f3f3e1ead25453f7d03ed0bf9
Zhou Mo <cdluminate@gmail.com>
2015-08-13 05:43:47 +0000

    update: glog.cpp

--- lang/cpp/glog.cpp
+++ lang/cpp/glog.cpp
@@ -7,11 +7,19 @@ main (int argc, char **argv) {
 	FLAGS_logtostderr = 1;
 	// init
 	google::InitGoogleLogging (argv[0]);
+
+	// conditional log
 	LOG_IF(INFO, 1 == FLAGS_logtostderr) << "FLAG logging to stderr is set";
+
+	// just log
 	LOG(INFO) << "Hello Glog ! ";
 	LOG(WARNING) << "warn";
-	PCHECK( 0 <= write(1,NULL,2) ) << "write() NULL failed";
 	LOG(ERROR) << "error";
+
+	// log and abort, dumping stack trace
+	CHECK( 0 == 1 ) << "?";
+	PCHECK( 0 <= write(1,NULL,2) ) << "write() NULL failed";
 	LOG(FATAL) << "fatal, then by default dump stack trace";
+
 	return 0;
 }
