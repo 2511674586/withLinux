4a5cb97976b308a0b2408388e278e2f1e0eb7ef0
Zhou Mo <cdluminate@gmail.com>
2016-02-05 04:37:56 +0000

    bn: cpp p p : update

--- booknote/cppprimerplus/codes/3.1.cc
+++ booknote/cppprimerplus/codes/3.1.cc
@@ -9,16 +9,20 @@ using std::endl;
 int main (void)
 {
 	cout << "Short size     (" << sizeof(short)  << ")" << TAB
-		<< "Short max      (" << SHRT_MAX  << ")" << endl;
+		<< "Short max      (" << SHRT_MAX  << ")" << TAB
+		<< "Short min      (" << SHRT_MIN  << ")" << endl;
 
 	cout << "Int size       (" << sizeof(int) << ")" << TAB
-		<< "Int max        (" << INT_MAX   << ")" << endl;
+		<< "Int max        (" << INT_MAX   << ")" << TAB
+		<< "Int min        (" << INT_MIN   << ")" << endl;
 
 	cout << "Long size      (" << sizeof(long) << ")" << TAB
-		<< "Long max       (" << LONG_MAX  << ")" << endl;
+		<< "Long max       (" << LONG_MAX  << ")" << TAB
+		<< "Long min       (" << LONG_MIN  << ")" << endl;
 
 	cout << "Long long size (" << sizeof(long long) << ")" << TAB
-		<< "Long long max  (" << LLONG_MAX << ")" << endl;
+		<< "Long long max  (" << LLONG_MAX << ")" << TAB
+		<< "Long long min  (" << LLONG_MIN << ")" << endl;
 
 	cout << "float size     (" << sizeof(float) << ")" << TAB << endl;
 	cout << "double size    (" << sizeof(double) << ")" << TAB << endl;
