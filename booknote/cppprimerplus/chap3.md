599fc947cf6bef396a6d7b7947a976cf8aeb5740
Zhou Mo <cdluminate@gmail.com>
2016-02-05 11:29:33 +0000

    bn: cpp p p : update chap3

--- booknote/cppprimerplus/chap3.md
+++ booknote/cppprimerplus/chap3.md
@@ -27,3 +27,33 @@ wchar_t x = L'P'; // prefix L for wide char constants
 wcout << L"tall" << endl;
 ```
 `char16_t` and `char32_t` are new C++11 types.
+```
+char16_t ch1 = u'q';
+char32_t ch2 = U'\U0000222B';
+```
+
+## bool type
+```
+bool flag = true;
+```
+
+## type conversion
+
+* type casts
+```
+(long) variable; // OK, straight C
+long (variable); // OK, pure C++
+```
+
+## auto declarations in C++11
+e.g. C++98 code might have this
+```
+std::vector<double> scores;
+std::vector<double>::iterator pv = scores.begin();
+```
+but C++11 allows one to write
+```
+std::vector<double> scores;
+auto pv = scores.begin();
+```
+
--- booknote/cppprimerplus/codes/3.1.cc
+++ booknote/cppprimerplus/codes/3.1.cc
@@ -1,32 +1,58 @@
 #include <iostream>
 #include <climits>
+#include <cfloat>
 
 using std::cout;
 using std::endl;
 
 #define TAB '\t'
+#define NEWLINE do {std::cout<<std::endl;} while(0)
 
 int main (void)
 {
 	cout << "Short size     (" << sizeof(short)  << ")" << TAB
 		<< "Short max      (" << SHRT_MAX  << ")" << TAB
 		<< "Short min      (" << SHRT_MIN  << ")" << endl;
+	NEWLINE;
 
 	cout << "Int size       (" << sizeof(int) << ")" << TAB
 		<< "Int max        (" << INT_MAX   << ")" << TAB
 		<< "Int min        (" << INT_MIN   << ")" << endl;
+	NEWLINE;
 
 	cout << "Long size      (" << sizeof(long) << ")" << TAB
 		<< "Long max       (" << LONG_MAX  << ")" << TAB
 		<< "Long min       (" << LONG_MIN  << ")" << endl;
+	NEWLINE;
 
 	cout << "Long long size (" << sizeof(long long) << ")" << TAB
 		<< "Long long max  (" << LLONG_MAX << ")" << TAB
 		<< "Long long min  (" << LLONG_MIN << ")" << endl;
+	NEWLINE;
 
-	cout << "float size     (" << sizeof(float) << ")" << TAB << endl;
-	cout << "double size    (" << sizeof(double) << ")" << TAB << endl;
-	cout << "long double size (" << sizeof(long double) << ")" << TAB << endl;
+	cout << "float size     (" << sizeof(float) << ")" << endl
+		<< "min num of significant digits (" << FLT_DIG << ")" << endl
+		<< "min exponent value (" << FLT_MIN_10_EXP << endl
+		<< "max exponent value (" << FLT_MAX_10_EXP << endl
+		<< "min (" << FLT_MIN << endl
+		<< "max (" << FLT_MAX << endl;
+	NEWLINE;
+
+	cout << "double size    (" << sizeof(double) << ")" << endl
+		<< "min num of significant digits (" << DBL_DIG << ")" << endl
+		<< "min exponent value (" << DBL_MIN_10_EXP << endl
+		<< "max exponent value (" << DBL_MAX_10_EXP << endl
+		<< "min (" << DBL_MIN << endl
+		<< "max (" << DBL_MAX << endl;
+	NEWLINE;
+
+	cout << "long double size (" << sizeof(long double) << ")" << endl
+		<< "min num of significant digits (" << LDBL_DIG << ")" << endl
+		<< "min exponent value (" << LDBL_MIN_10_EXP << endl
+		<< "max exponent value (" << LDBL_MAX_10_EXP << endl
+		<< "min (" << LDBL_MIN << endl
+		<< "max (" << LDBL_MAX << endl;
+	NEWLINE;
 
 	return 0;
 }
