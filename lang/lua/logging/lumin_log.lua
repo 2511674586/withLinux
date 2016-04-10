c261a26f546e60f68c4fd79841a292e753b256a2
Zhou Mo <cdluminate@gmail.com>
2016-04-10 07:33:26 +0000

    lang:lua: logging: update

--- lang/lua/logging/lumin_log.lua
+++ lang/lua/logging/lumin_log.lua
@@ -2,6 +2,8 @@
 -- Lumin's lua Logging facility module
 -- COPYRIGHT (C) 2016 Lumin Zhou
 -- MIT LICENSE
+-- changelog:
+--   mar 13 2016, add alias for all public functions
 
 local LOG = {}
 function LOG.__CORE (level, dt, di, message)
@@ -17,24 +19,41 @@ function LOG._CORE (level, message, ending)
 	if dt == nil then return end
 	LOG.__CORE (level, dt, di, message .. ending)
 end
+
 function LOG.DEBUG (message)
 	io.write ('\x1b[36;1m')
 	LOG._CORE ('D', message)
 	io.write ('\x1b[m')
 end
+function LOG.debug (message)
+	return LOG.DEBUG (message)
+end
+
 function LOG.INFO (message)
 	io.write ('\x1b[32;1m')
 	LOG._CORE ('I', message)
 	io.write ('\x1b[m')
 end
+function LOG.info (message)
+	return LOG.INFO (message)
+end
+
 function LOG.WARN (message)
 	io.write ('\x1b[33;1m')
 	LOG._CORE ('W', message)
 	io.write ('\x1b[m')
 end
+function LOG.warn (message)
+	return LOG.WARN (message)
+end
+
 function LOG.ERROR (message)
 	io.write ('\x1b[31;1m')
 	LOG._CORE ('E', message)
 	io.write ('\x1b[m')
 end
+function LOG.error (message)
+	return LOG.ERROR (message)
+end
+
 return LOG
